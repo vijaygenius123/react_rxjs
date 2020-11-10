bde8e979857c99f42d423c5c9f403ef041249ddb
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-10 22:45:42 +0530

    Counting Down From 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import {interval} from "rxjs";
+import {scan, startWith, takeWhile} from "rxjs/operators";
 
-const observable$ = interval(1000)
+const observable$ = interval(1000).pipe(
+    startWith(5),
+    scan(time => time - 1 ),
+    takeWhile(time => time > 0)
+)
 
 function App() {
     const [state, setState] = useState();
     useEffect(()=> {
-    observable$.subscribe(setState)
+    const sub = observable$.subscribe(setState);
+    return () => sub.unsubscribe();
     },[])
     return (
         <div className="App">
