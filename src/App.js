c2ed0a134d0ec2fc6d869f184c6b4398e17d9c9e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-10 22:40:52 +0530

    Used interval and useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {interval} from "rxjs";
+
+const observable$ = interval(1000)
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const [state, setState] = useState();
+    useEffect(()=> {
+    observable$.subscribe(setState)
+    },[])
+    return (
+        <div className="App">
+            <h3>Alarm Clock</h3>
+          <div className="display">{state}</div>
+        </div>
+    );
 }
 
 export default App;
