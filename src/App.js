f945dd1e3fd72b3897754357994347aec78f5b65
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-11 21:17:31 +0530

    Using share

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import {concat, interval, of, Subject} from "rxjs";
-import {scan, startWith, takeWhile} from "rxjs/operators";
+import {repeatWhen, scan, share, startWith, takeWhile} from "rxjs/operators";
 
-const countDown$ = interval(1000).pipe(
+const countDown$ = interval(1000)
+    .pipe(
     startWith(5),
-    scan(time => time - 1 ),
+    scan(time => time - 1),
     takeWhile(time => time > 0)
 )
+    .pipe(share());
 
-const observable$ = concat(countDown$, of("Wake Up"));
+const observable$ = concat(countDown$, of("Wake Up")).pipe(
+    repeatWhen(() => action$)
+)
 
 const action$ = new Subject();
 
-action$.subscribe(console.log)
 
 function App() {
     const [state, setState] = useState();
-    useEffect(()=> {
-    const sub = observable$.subscribe(setState);
-    return () => sub.unsubscribe();
-    },[])
+    useEffect(() => {
+        const sub = observable$.subscribe(setState);
+        return () => sub.unsubscribe();
+    }, [])
     return (
         <div className="App">
             <h3>Alarm Clock</h3>
-          <div className="display">{state}</div>
+            <div className="display">{state}</div>
             <button className="snooze" onClick={() => action$.next('snooze')}>Snooze</button>
         </div>
     );
